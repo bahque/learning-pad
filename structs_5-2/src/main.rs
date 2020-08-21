ea69c15cea9995d0cba97ba538c637a4b2fbaf48
bahque <bahque@gmail.com>
2020-08-21 20:25:52 +0000

    versione con la tupla

--- structs_5-2/src/main.rs
+++ structs_5-2/src/main.rs
@@ -1,14 +1,15 @@
 fn main() {
     let w1 = 30;
     let h1 = 50;
+    let rect = (w1, h1);
 
     println!(
         "area is {}",
-        area(w1, h1)
+        area(rect)
     )
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    width * height
+fn area(rect: (u32, u32)) -> u32 {
+    rect.0 * rect.1
 }
 
