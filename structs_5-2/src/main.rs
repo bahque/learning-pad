7c16fae65e3af1343d9cacab22375671a154dad9
bahque <bahque@gmail.com>
2020-08-21 16:26:34 +0000

    primo programmino rust

--- structs_5-2/src/main.rs
+++ structs_5-2/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    let w1 = 30;
+    let h1 = 50;
+
+    println!(
+        "area is {}",
+        area(w1, h1)
+    )
 }
+
+fn area(width: u32, height: u32) -> u32 {
+    width * height
+}
+
